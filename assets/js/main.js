6009a811cedff8c22e272c1ea900c9b48ef1ec3e
Nicola-Perera <nicola.perera96@mail.com>
2021-10-21 00:49:37 +0200

    created grid for level 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,14 +49,11 @@ console.log(newGrid);
 const gridRef = document.getElementById('grid');
 
 for (let i = 1; i <= newGrid; i++) {
+    
     const cell = document.createElement('div');
-    const cellElement = `<div class="cell"></div>`;
-    cell.innerHTML = cellElement;
-    const cellArea = Math.sqrt(newGrid);
-    cell.style.width = 1 / Math.sqrt;
-    cell.style.heigth = 1 / cellArea;
-    console.log(cell);
-
+    cell.className = 'grid_cell';
+    
     gridRef.append(cell);
+    
 }
 
