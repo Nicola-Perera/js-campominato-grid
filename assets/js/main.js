cc79c7277fadbfc89de19210a7847782524a5ef7
Nicola-Perera <nicola.perera96@mail.com>
2021-10-20 16:00:22 +0200

    add steps to complete the software

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,12 @@
             con difficoltà 3 => tra 1 e 49
         Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
+
+/*
+    STEPS:
+        chiedo all'utente di inserire un numero che indichi il livello di difficoltà al quale vuole giocare;
+        uso il numero inserito dall'utente come indice per un ciclo for che crea una cella:
+            numerata in base all'indice in cui si trova il ciclo;
+            si colora di azzurro quando ci si clicca sopra;
+        inserisco ogni cella in una griglia quadrata;
+*/
